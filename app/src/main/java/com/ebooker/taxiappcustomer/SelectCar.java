2f295bfd2512e86049bf57beb06c01b0a58a86ca
anshul <huntinggamingtech@gmail.com>
2021-07-31 22:12:44 +0530

    Todays Work Done

---
+++ app/src/main/java/com/ebooker/taxiappcustomer/SelectCar.java
@@ -0,0 +1,14 @@
+package com.ebooker.taxiappcustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectCar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_car);
+    }
+}
