9b969437d93fe9d181b74ccc9ec7aff2c2fd6ee3
anshul <huntinggamingtech@gmail.com>
2021-08-08 14:54:05 +0530

    Todays Work Done

--- app/src/main/java/com/ebooker/taxiappcustomer/FromTo.java
+++ app/src/main/java/com/ebooker/taxiappcustomer/FromTo.java
@@ -1,14 +1,72 @@
 package com.ebooker.taxiappcustomer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class FromTo extends AppCompatActivity {
+
+    private TextView fromAddress, toAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from_to);
+        fromAddress=findViewById(R.id.from_to_from_address);
+        toAddress=findViewById(R.id.from_to_to_addresss);
+        Places.initialize(this,"AIzaSyALB-2ynAJ7F0K2-q2CQkaVp-HVg5ynkM0");
+
+        fromAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fieldList= Arrays.asList(Place.Field.ADDRESS,
+                        Place.Field.LAT_LNG, Place.Field.NAME);
+                Intent intent=new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(FromTo.this);
+                startActivityForResult(intent,100);
+            }
+        });
+        toAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fieldList= Arrays.asList(Place.Field.ADDRESS,
+                        Place.Field.LAT_LNG, Place.Field.NAME);
+                Intent intent=new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(FromTo.this);
+                startActivityForResult(intent,200);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            fromAddress.setText(place.getAddress());
+        }
+        else if(requestCode == 200 && resultCode == RESULT_OK){
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            toAddress.setText(place.getAddress());
+
+        }
+        else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Log.d("anshulnegi",status.toString());
+        }
+    }
+
 }
